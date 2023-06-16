74020ce7fd0938618f5615d720f5af1d3b21caab
Divyani Yadav <43127091+divs128@users.noreply.github.com>
2023-06-16 11:03:00 +0530

    Update address.py

--- models/address.py
+++ models/address.py
@@ -1,6 +1,17 @@
 from database.database import cursor
 """
-Represents the address table and provides methods for retrieving address details
+Represents the address table and provides methods for retrieving address details.
+
+Attributes:
+- id: The ID of the address
+- street1: The first line of the address
+- street2: The second line of the address
+- city: The city of the address
+- zip: The ZIP code of the address
+
+Methods:
+- get_address_by_id(address_id): Retrieves an address object by its ID from the database
+
 """
 class Address:
     def __init__(self, id, street1, street2, city, zip):
@@ -12,6 +23,16 @@ class Address:
 
     @staticmethod
     def get_address_by_id(address_id):
+         """
+        Retrieves an address object by its ID from the database.
+
+        Parameters:
+        - address_id: The ID of the address to retrieve
+
+        Returns:
+        - An Address object if the address is found, None otherwise
+        """
+
         query = "SELECT id, street1, street2, city, zip FROM address WHERE id = %s"
         cursor.execute(query, (address_id,))
         result = cursor.fetchone()
