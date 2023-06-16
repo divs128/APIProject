70070df1df206233c6c7da67abef4c3ba1d615ae
Divyani Yadav <43127091+divs128@users.noreply.github.com>
2023-06-16 11:17:19 +0530

    Update user_provider.py

--- models/user_provider.py
+++ models/user_provider.py
@@ -1,8 +1,14 @@
 from database.database import cursor
-
 """
-Represents the userprovider table and provides methods for
-retrieving the user-provider relationship
+Represents the user-provider relationship and provides methods for retrieving user-provider details.
+
+Attributes:
+- id: The ID of the user-provider relationship
+- user_id: The ID of the user
+- provider_id: The ID of the provider
+
+Methods:
+- get_user_provider_by_user_id(user_id): Retrieves the user-provider relationship for the given user ID
 
 """
 
@@ -15,6 +21,15 @@ class UserProvider:
 
     @staticmethod
     def get_user_provider_by_user_id(user_id):
+        """
+        Retrieves the user-provider relationship for the given user ID.
+
+        Parameters:
+        - user_id: The ID of the user
+
+        Returns:
+        - The UserProvider object if the user-provider relationship exists, None otherwise
+        """
         query = "SELECT id, user_id, provider_id FROM userprovider WHERE user_id = %s"
         cursor.execute(query, (user_id,))
         result = cursor.fetchone()
