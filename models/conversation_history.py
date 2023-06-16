79f12c8c0da1e4c2790a4b69662b68c1cce762ef
Divyani Yadav <43127091+divs128@users.noreply.github.com>
2023-06-16 11:09:13 +0530

    Update conversation_history.py

--- models/conversation_history.py
+++ models/conversation_history.py
@@ -1,6 +1,17 @@
 from database.database import cursor, conn
 """
 Represents the conversationhistory table and provides methods for storing conversation history.
+
+Attributes:
+- id: The ID of the conversation history entry
+- conversation_id: The ID of the conversation associated with the history entry
+- question: The question in the conversation
+- answer: The answer in the conversation
+- created_on: The timestamp indicating when the history entry was created
+
+Methods:
+- create_conversation_history(conversation_id, question, answer): Stores a new conversation history entry in the database
+
 """
 class ConversationHistory:
     def __init__(self, id, conversation_id, question, answer, created_on):
@@ -12,6 +23,17 @@ class ConversationHistory:
 
     @staticmethod
     def create_conversation_history(conversation_id, question, answer):
+        """
+        Stores a new conversation history entry in the database.
+
+        Parameters:
+        - conversation_id: The ID of the conversation associated with the history entry
+        - question: The question in the conversation
+        - answer: The answer in the conversation
+
+        Returns:
+        - The ID of the created conversation history entry
+        """
         try:
             query = "INSERT INTO conversationhistory (conversation_id, question, answer, created_on) VALUES (%s, %s, " \
                     "%s, NOW())"
